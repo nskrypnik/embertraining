2981259786e8fecb6def0c2830a6f38c5ad3c676
Matthew Dahl <mdahl@ciena.com>
2016-05-10 11:57:06 -0700

    Mock organizations and template

--- app/pods/organizations/route.js
+++ app/pods/organizations/route.js
@@ -2,4 +2,16 @@ import Ember from 'ember'
 const {Route} = Ember
 
 export default Route.extend({
+  model () {
+    return [
+      {
+        id: 1,
+        name: 'Org 1'
+      },
+      {
+        id: 2,
+        name: 'Org 2'
+      }
+    ]
+  }
 })
