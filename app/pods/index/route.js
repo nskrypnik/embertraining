935c68c1464ccc9f91b79638bed8015b00b5f7cb
Matthew Dahl <mdahl@ciena.com>
2016-05-10 11:22:42 -0700

    Add JSDoc

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -4,6 +4,9 @@ const {Route} = Ember
 export const transitionToRouteName = 'organizations'
 
 export default Route.extend({
+  /**
+   * Transition to route for desired application entry point
+   */
   beforeModel () {
     this._super(...arguments)
     this.transitionTo(transitionToRouteName)
